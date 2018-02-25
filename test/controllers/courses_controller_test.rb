61988aa251cb1e8f1444fb16f33f8e62d91f055b
gearsgrinding <hualex@seas.upenn.edu>
2018-02-24 22:20:54 -0500

    more styling

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -4,32 +4,4 @@ class CoursesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-
-  setup do
-  	@student = @course.subscriptions.first.student
-  	@teacher = @course.teacher
-  	@admin = Admin.first
-  	@course = Course.first
-  end
-
-  test "index for student" do
-    sign_in(@student)
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:courses)
-  end
-  
-  test "index for admin" do
-    sign_in(@admin)
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:courses)
-  end
-  
-  test "index for teacher" do
-    sign_in(@teacher)
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:courses)
-  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
 	def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com" , password: "test12")
   end
 
   test "should be valid" do
     assert @user.valid?
   end
-  # test "the truth" do
-  #   assert true
-  # end
+  
 end
